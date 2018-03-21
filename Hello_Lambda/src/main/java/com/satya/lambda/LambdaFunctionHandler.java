d84a849bbbd0572fc5a6eddcd41dd86e01acfb73
17satya <satyendra.singh@bridgelabz.com>
2018-03-21 17:38:25 +0530

    hello lambda

---
+++ Hello_Lambda/src/main/java/com/satya/lambda/LambdaFunctionHandler.java
@@ -0,0 +1,17 @@
+package com.satya.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> 
+{
+
+    @Override
+    public String handleRequest(String input, Context context) 
+    {
+        context.getLogger().log("Input: " + input);
+
+        return "Hello - "+ input+ " welcome to Lambda!";
+    }
+
+}
